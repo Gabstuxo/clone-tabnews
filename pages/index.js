8d93c8b9d6a816ae0b277ceaa8afabfe67bf6232
Gabriel Feronelly <gabriel.landrad03@gmail.com>
2026-03-13 00:26:10 -0300

    Add initial project structure

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Clone do TabNews!</h1>
+      <p>Este é um projeto de clone do TabNews usando Next.js.</p>
+    </div>
+  );
+}
+
+export default Home;
